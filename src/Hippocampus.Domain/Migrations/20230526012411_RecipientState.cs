11ec012176ea51e32f6e90e61f981d689bc1280f
BrenoReisSantos <breno.battle@gmail.com>
2023-05-25 22:27:11 -0300

    chore: mudança no nome da coluna de State para RecipientState

---
+++ src/Hippocampus.Domain/Migrations/20230526012411_RecipientState.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hippocampus.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class RecipientState : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "State",
+                table: "RecipientLogs",
+                newName: "RecipientState");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RecipientState",
+                table: "RecipientLogs",
+                newName: "State");
+        }
+    }
+}
